265ec004489c92ae9a54c6e5accd51dc65bd657d
Marat <milushev.marat@yandex.com>
2022-06-29 09:20:08 +0500

    p14test

--- app.js
+++ app.js
@@ -1,44 +1,61 @@
-const bcrypt = require('bcryptjs');
-const express = require('express');
+const bcrypt = require("bcryptjs");
+const express = require("express");
 const { PORT = 3000, BASE_PATH } = process.env;
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const app = express();
-const bodyParser = require('body-parser');
-const { createUser, login } = require('./controllers/users');
-const auth = require('./middlewares/auth');
+const bodyParser = require("body-parser");
+const { createUser, login } = require("./controllers/users");
+const auth = require("./middlewares/auth");
+const { celebrate, Joi } = require("celebrate");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true
+mongoose.connect("mongodb://localhost:27017/mestodb", {
+  useNewUrlParser: true,
 });
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post(
+  "/signin",
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
+  }),
+  login
+);
+app.post(
+  "/signup",
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().regex(/^(https?:\/\/)?(www\.)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-_~:/?#[]@!$&'()*,;=]*)*\/?$/),
+      email: Joi.string().required().email(),
+      password: Joi.string().required().min(8),
+    }),
+  }),
+  createUser
+);
 
 app.use(auth);
 
-app.use('/users', require('./routes/users'));
-app.use('/cards', require('./routes/cards'));
-app.use('*', (req, res) => {
-  res.status(404).send({ message: "Страница не найдена"});
+app.use("/users", require("./routes/users"));
+app.use("/cards", require("./routes/cards"));
+app.use("*", (req, res) => {
+  res.status(404).send({ message: "Страница не найдена" });
 });
 
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
 
-  res
-    .status(statusCode)
-    .send({
-      message: statusCode === 500
-        ? 'На сервере произошла ошибка'
-        : message
-    });
+  res.status(statusCode).send({
+    message: statusCode === 500 ? "На сервере произошла ошибка" : message,
+  });
 });
 
-
 app.listen(PORT, () => {
-  console.log('Ссылка на сервер');
+  console.log("Ссылка на сервер");
   console.log(BASE_PATH);
 });
--- routes/users.js
+++ routes/users.js
@@ -33,7 +33,7 @@ router.patch(
   "/me/avatar",
   celebrate({
     body: Joi.object().keys({
-      avatar: Joi.string().required().min(2).max(30),
+      avatar: Joi.string().regex(/^(https?:\/\/)?(www\.)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-_~:/?#[]@!$&'()*,;=]*)*\/?$/),
     }),
   }),
   newAvatar
